933a1a06a192019a85367ca5ae09202aea2a0f1b
Yadhav VR <yadhavvr96@gmail.com>
2024-06-19 11:49:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import TodoInput from "./Todoinput";
+import TodoList from "./Todolist";
+import "./App.css";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>React Todo App</h1>
+      <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+      <TodoList list={todos} remove={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
